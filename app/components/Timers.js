d0271db7c58c4eaad3ef84d18fe9e7e81cbbdf0e
Eamon mikulec <boogie@mikulec.com>
2018-03-07 16:25:30 -0800

    Sort timers alphabetically

--- app/components/Timers.js
+++ app/components/Timers.js
@@ -3,6 +3,7 @@ import React from "react";
 
 import Timer from "./Timer";
 import type { TimerState } from "../reducers/timer";
+import type { Timer as TimerType } from "../types/Timer";
 import TimersContainer, { Item } from "./TimersStyles";
 
 export type Props = {
@@ -13,14 +14,13 @@ export type Props = {
 
 export default (props: Props) => {
   // See https://github.com/facebook/flow/issues/2221
-  const TimerComps = Object.keys(props.timers).map(timerId => {
-    const timer = props.timers[timerId];
-    return (
+  const TimerComps = Object.values(props.timers) // $FlowIssue
+    .sort((a: TimerType, b: TimerType) => a.name.localeCompare(b.name)) // $FlowIssue
+    .map((timer: TimerType) => (
       <Item key={timer.id}>
         <Timer play={props.play} pause={props.pause} timer={timer} />
       </Item>
-    );
-  });
+    ));
 
   return <TimersContainer>{TimerComps}</TimersContainer>;
 };
