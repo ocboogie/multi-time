5fd8f294df17e9bcf73532f6bb1e943780690151
Eamon mikulec <boogie@mikulec.com>
2018-03-08 04:50:22 -0800

    Remove unnecessary dependencies

--- app/index.js
+++ app/index.js
@@ -2,8 +2,6 @@ import React from "react";
 import { render } from "react-dom";
 import uuid from "uuid/v4";
 import "@fortawesome/fontawesome";
-import "web-animations-js";
-import "hammerjs";
 import "materialize-css";
 import "materialize-css/dist/css/materialize.min.css";
 
