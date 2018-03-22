957dfb98ad4e360d7c2d621fb916a862ad1db7eb
Boogie eamon <boogie@mikulec.com>
2018-03-21 21:25:05 -0700

    Use generateTimer for initial timers

--- app/index.js
+++ app/index.js
@@ -1,27 +1,23 @@
 // @flow
 import React from "react";
 import { render } from "react-dom";
-import uuid from "uuid/v4";
 import "@fortawesome/fontawesome";
 import "materialize-css";
 import "materialize-css/dist/css/materialize.min.css";
 
 import Root from "./containers/Root";
 import { configureStore } from "./store";
-import { addTimer } from "./actions/timer";
-import type { State } from "./types/State";
+import { generateTimer } from "./actions/timer";
 import type { Store } from "./types/Store";
 import "./style";
 
-const state: State = { timer: [] };
+const store: Store = configureStore();
 
-const store: Store = configureStore(state);
-
-store.dispatch(addTimer({ time: 0, name: "first", paused: true, id: uuid() }));
-store.dispatch(addTimer({ time: 0, name: "second", paused: true, id: uuid() }));
-store.dispatch(addTimer({ time: 0, name: "third", paused: true, id: uuid() }));
-store.dispatch(addTimer({ time: 0, name: "forth", paused: true, id: uuid() }));
-store.dispatch(addTimer({ time: 0, name: "fifth", paused: true, id: uuid() }));
+store.dispatch(generateTimer({ name: "first" }));
+store.dispatch(generateTimer({ name: "second" }));
+store.dispatch(generateTimer({ name: "third" }));
+store.dispatch(generateTimer({ name: "forth" }));
+store.dispatch(generateTimer({ name: "fifth" }));
 
 // $FlowFixMe
 render(<Root store={store} />, document.getElementById("root"));
