415909ab60131ede0ba20153eb6b8eda53fcc429
Boogie eamon <boogie@mikulec.com>
2018-03-21 20:49:27 -0700

    Rework redux types

--- app/types/Store.js
+++ app/types/Store.js
@@ -4,11 +4,8 @@ import type { Store as ReduxStore, Dispatch as ReduxDispatch } from "redux";
 import type { Action } from "./Action";
 import type { State } from "./State";
 
-export type Store = ReduxStore<State, Action>;
-
 export type GetState = () => State;
-
 // eslint-disable-next-line no-use-before-define
-export type Dispatch = ReduxDispatch<Action> & Thunk;
-
-export type Thunk = ((Dispatch, GetState) => Promise<void> | void) => any;
+export type Dispatch = ReduxDispatch<Action> & Thunk<Action>;
+export type Thunk<A> = ((Dispatch, GetState) => Promise<void> | void) => A;
+export type Store = ReduxStore<State, Action, Dispatch>;
