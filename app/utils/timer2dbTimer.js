ecb50b303fae5b4b171f1119ba3be6f709fefaaa
ocboogie <boogie@mikulec.com>
2018-06-13 18:46:17 -0700

    Improve the timer2dbTimer function

--- app/utils/timer2dbTimer.js
+++ app/utils/timer2dbTimer.js
@@ -5,7 +5,7 @@ import getElapsedTime from "./getElapsedTime";
 export default function timer2dbTimer(timer: Timer): DbTimer {
   return {
     id: timer.id,
-    name: timer.name,
+    name: timer.name || "",
     elapsed: getElapsedTime(
       timer.timing.baseTime,
       timer.timing.startedAt,
