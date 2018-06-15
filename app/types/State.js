c932532851c436e19768fbcf8e91742f94b601f0
ocboogie <boogie@mikulec.com>
2018-06-14 17:38:18 -0700

    Fix typo

--- app/types/State.js
+++ app/types/State.js
@@ -1,11 +1,11 @@
 // @flow
-import type { authState } from "../reducers/auth";
+import type { AuthState } from "../reducers/auth";
 import type { ModalState } from "../reducers/modal";
 import type { TimerState } from "../reducers/timer";
 import type { TrashState } from "../reducers/trash";
 
 export type State = {
-  +auth: authState,
+  +auth: AuthState,
   +modal: ModalState,
   +timer: TimerState,
   +trash: TrashState
