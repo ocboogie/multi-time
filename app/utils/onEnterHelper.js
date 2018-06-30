8faf686c50ce5c34865f364e5fd4d9f4c03008e9
ocboogie <boogie@mikulec.com>
2018-06-30 14:39:48 -0700

    Remove unused file

--- app/utils/onEnterHelper.js
+++
@@ -1,9 +0,0 @@
-// @flow
-
-// eslint-disable-next-line no-undef
-export default (event: SyntheticKeyboardEvent<HTMLAnchorElement>) => {
-  if (event.key === "Enter") {
-    // $FlowIssue
-    event.target.click();
-  }
-};
